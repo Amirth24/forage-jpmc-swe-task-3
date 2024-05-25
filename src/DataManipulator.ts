d0b64a02479ade238ba1e745ac55b63a35e25c39
Amirthanathan R <amirth300324@gmail.com>
2024-05-25 21:43:45 +0530

    Ratio dashboard

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,33 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
   timestamp: Date,
+  trigger_alert: undefined | number,
+  price_abc: number,
+  price_def: number,
+  ratio: number,
+  lower_bound: number,
+  upper_bound: number,
 }
 
 
 export class DataManipulator {
   static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+    const price_abc = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price ) / 2.0;
+    const price_def = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price ) / 2.0;
+    const ratio = price_def === 0.0 ? 0.0 : price_abc/ price_def;
+    const lower_bound = 1 - 0.05;
+    const upper_bound = 1 + 0.05;
+
+    return {
+        price_abc,
+        price_def,
+        ratio,
+        timestamp: serverResponds[0].timestamp > serverResponds[1].timestamp ?
+          serverResponds[0].timestamp : serverResponds[0].timestamp,
+        lower_bound,
+        upper_bound,
+        trigger_alert: (ratio < lower_bound || ratio >= upper_bound) ? ratio : undefined
+    };
   }
 }
